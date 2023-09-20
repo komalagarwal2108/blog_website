27b9a65a4920f27a8c3d8619a1d6983b921af8ce
Komal Agarwal <komalagarwal2108@gmail.com>
2023-09-20 11:23:07 +0530

    express-cors

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require("cors");
+const cors = require("express-cors");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
